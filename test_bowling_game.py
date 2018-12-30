009a5d3203af4bb4f477f64bd60a5e4393a3d4b5
Shahrokh Bahtooei <Shahrokh.Bahtuie@gmail.com>
2018-12-30 19:50:27 +0330

    2-1 Test

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -7,6 +7,10 @@ class TestGame(unittest.TestCase):
     def test_can_create_game(self):
         g = Game()
 
+    def test_can_roll(self):
+        g = Game()
+        g.roll(0)
+
 
 if __name__ == '__main__':
     unittest.main()

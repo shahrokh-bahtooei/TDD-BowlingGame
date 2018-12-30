40403f556eaf992ac80b43baa088bd896f0a91d8
Shahrokh Bahtooei <Shahrokh.Bahtuie@gmail.com>
2018-12-30 20:10:33 +0330

    2-3 Refactor

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -4,12 +4,11 @@ from bowling_game import Game
 
 class TestGame(unittest.TestCase):
 
-    def test_can_create_game(self):
-        g = Game()
+    def setUp(self):
+        self.g = Game()
 
     def test_can_roll(self):
-        g = Game()
-        g.roll(0)
+        self.g.roll(0)
 
 
 if __name__ == '__main__':

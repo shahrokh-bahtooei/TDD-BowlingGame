6ea26abe066fcf1ce1b0101908f3804828637b5c
Shahrokh Bahtooei <Shahrokh.Bahtuie@gmail.com>
2018-12-30 20:25:30 +0330

    3-1 Test

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -10,6 +10,11 @@ class TestGame(unittest.TestCase):
     def test_can_roll(self):
         self.g.roll(0)
 
+    def test_gutter_game(self):
+        for i in range(20):
+            self.g.roll(0)
+        self.assertEqual(0, self.g.score())
+
 
 if __name__ == '__main__':
     unittest.main()

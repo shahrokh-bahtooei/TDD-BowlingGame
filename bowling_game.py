4f20c8b31253bf82a645eda896f8e826ea4dc33d
Shahrokh Bahtooei <Shahrokh.Bahtuie@gmail.com>
2019-01-01 22:20:08 +0330

    6-3 Refactor

--- bowling_game.py
+++ bowling_game.py
@@ -2,10 +2,12 @@
 class Game(object):
 
     def __init__(self):
-        self._rolls = []
+        self._rolls = [0] * 21
+        self._current_roll = 0
 
     def roll(self, pins):
-        self._rolls.append(pins)
+        self._rolls[self._current_roll] = pins
+        self._current_roll += 1
 
     def score(self):
         score = 0

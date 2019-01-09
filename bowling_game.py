b3b19f3f1a2c3cc7794711b278163c38ae1a5236
Shahrokh Bahtooei <Shahrokh.Bahtuie@gmail.com>
2019-01-09 14:59:01 +0330

    Raising errors for invalid rolls are developed.

--- bowling_game.py
+++ bowling_game.py
@@ -2,14 +2,16 @@
 class Game(object):
 
     def __init__(self):
-        self._rolls = [0] * 21
+        self._rolls = [-1] * 21
         self._current_roll = 0
 
-    def roll(self, pins):
+    def roll(self, pins: int):
+        self._raise_error_if_invalid_roll(pins)
+
         self._rolls[self._current_roll] = pins
         self._current_roll += 1
 
-    def score(self):
+    def score(self) -> int:
         score = 0
         first_in_frame = 0
         for frame in range(10):
@@ -26,17 +28,92 @@ class Game(object):
                 first_in_frame += 2
         return score
 
+    def _raise_error_if_invalid_roll(self, pins):
+        self._raise_error_if_out_of_scope_roll(pins)
+        self._raise_error_if_more_than_10_pins_rolled_in_this_frame(pins)
+        self._raise_error_if_rolled_more_than_21_times()
+        self._raise_error_if_rolled_after_2_non_bonus_rolls_in_10th_frame()
+
+    @staticmethod
+    def _raise_error_if_out_of_scope_roll(pins):
+        if pins > 10 or pins < 0:
+            raise OutOfScopeRoll
+
+    def _raise_error_if_more_than_10_pins_rolled_in_this_frame(self, pins):
+        if self._is_2nd_roll_in_frame():
+            if self._more_than_10_pins_are_rolled_in_this_frame(pins):
+                raise MoreThan10PinsRolledInAFrame
+
+    def _is_2nd_roll_in_frame(self):
+        return (self._current_roll - self._count_strikes()) % 2 == 1
+
+    def _count_strikes(self):
+        strikes_count = 0
+        for roll_num in range(0, self._current_roll):
+            if self._rolls[roll_num] == 10:
+                strikes_count += 1
+        return strikes_count
+
+    def _more_than_10_pins_are_rolled_in_this_frame(self, pins):
+        return self._rolls[self._current_roll - 1] + pins > 10
+
+    def _raise_error_if_rolled_more_than_21_times(self):
+        if self._rolled_more_than_21_times():
+            raise RolledAfter10thFrame
+
+    def _rolled_more_than_21_times(self):
+        return self._current_roll > 20
+
+    def _raise_error_if_rolled_after_2_non_bonus_rolls_in_10th_frame(self):
+        if self._is_3rd_roll_of_10th_frame():
+            has_bonus_for_strike = self._is_strike(self._current_roll - 2)
+            has_bonus_for_spare = self._is_spare(self._current_roll - 2)
+            if not has_bonus_for_spare and not has_bonus_for_strike:
+                raise RolledAfter10thFrame
+
+    def _is_3rd_roll_of_10th_frame(self):
+        first_in_frame = 0
+        frame = 1
+
+        while frame < 10 and first_in_frame + 2 < self._current_roll:
+
+            if self._is_strike(first_in_frame):
+                first_in_frame += 1
+                frame += 1
+            else:
+                first_in_frame += 2
+                frame += 1
+
+        return frame == 10 and first_in_frame + 2 == self._current_roll
+
     def _is_strike(self, first_in_frame):
         return self._rolls[first_in_frame] == 10
 
-    def _next_two_balls_for_strike(self, first_in_frame):
-        return self._rolls[first_in_frame + 1] + self._rolls[first_in_frame + 2]
-
     def _is_spare(self, first_in_frame):
         return self._rolls[first_in_frame] + self._rolls[first_in_frame + 1] == 10
+
+    def _next_two_balls_for_strike(self, first_in_frame):
+        return self._rolls[first_in_frame + 1] + self._rolls[first_in_frame + 2]
 
     def _next_ball_for_spare(self, first_in_frame):
         return self._rolls[first_in_frame + 2]
 
     def _two_balls_in_frame(self, first_in_frame):
         return self._rolls[first_in_frame] + self._rolls[first_in_frame + 1]
+
+
+class OutOfScopeRoll(RuntimeError):
+    pass
+
+
+class MoreThan10PinsRolledInAFrame(RuntimeError):
+    pass
+
+
+class RolledAfter10thFrame(RuntimeError):
+    pass
+
+
+class GameIsNotFinishedDueToNotEnoughRolls(RuntimeError):
+    pass
+
--- test_bowling_game.py
+++ test_bowling_game.py
@@ -1,5 +1,6 @@
 import unittest
-from bowling_game import Game
+from bowling_game import Game, \
+    OutOfScopeRoll, MoreThan10PinsRolledInAFrame, RolledAfter10thFrame, GameIsNotFinishedDueToNotEnoughRolls
 
 
 class TestGame(unittest.TestCase):
@@ -42,6 +43,40 @@ class TestGame(unittest.TestCase):
     def test_perfect_game(self):
         self.roll_many(12, 10)
         self.assertEqual(self.g.score(), 300)
+
+    def test_roll_of_greater_than_10_raises_error(self):
+        with self.assertRaises(OutOfScopeRoll):
+            self.g.roll(11)
+
+    def test_negative_roll_raises_error(self):
+        with self.assertRaises(OutOfScopeRoll):
+            self.g.roll(-1)
+
+    def test_frame_rolls_in_sum_greater_than_10_raises_error(self):
+        self.roll_strike()
+        self.roll_many(2, 4)
+        self.g.roll(1)
+        with self.assertRaises(MoreThan10PinsRolledInAFrame):
+            self.g.roll(10)
+
+    def test_more_than_21_rolls_raises_error(self):
+        self.roll_many(18, 0)
+        self.roll_spare()
+        self.g.roll(0)
+        with self.assertRaises(RolledAfter10thFrame):
+            self.g.roll(3)
+
+    def test_a_roll_after_2_non_bonus_rolls_in_10th_frame_raises_error(self):
+        self.roll_many(9, 10)
+        self.roll_many(2, 1)
+        with self.assertRaises(RolledAfter10thFrame):
+            self.g.roll(8)
+
+    @unittest.skip
+    def test_calling_score_before_12_strikes_raises_error(self):
+        self.roll_many(11, 10)
+        with self.assertRaises(GameIsNotFinishedDueToNotEnoughRolls):
+            self.g.score()
 
 
 if __name__ == '__main__':

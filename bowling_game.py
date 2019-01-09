876171b93fbf403ff4d5dee863aa0718f622718b
Shahrokh Bahtooei <Shahrokh.Bahtuie@gmail.com>
2019-01-09 15:27:05 +0330

    Refactored to be more Pythonic.

--- bowling_game.py
+++ bowling_game.py
@@ -2,14 +2,11 @@
 class Game(object):
 
     def __init__(self):
-        self._rolls = [-1] * 21
-        self._current_roll = 0
+        self._rolls = []
 
     def roll(self, pins: int):
         self._raise_error_if_invalid_roll(pins)
-
-        self._rolls[self._current_roll] = pins
-        self._current_roll += 1
+        self._rolls.append(pins)
 
     def score(self) -> int:
         score = 0
@@ -45,29 +42,29 @@ class Game(object):
                 raise MoreThan10PinsRolledInAFrame
 
     def _is_2nd_roll_in_frame(self):
-        return (self._current_roll - self._count_strikes()) % 2 == 1
+        return (len(self._rolls) - self._count_strikes()) % 2 == 1
 
     def _count_strikes(self):
         strikes_count = 0
-        for roll_num in range(0, self._current_roll):
-            if self._rolls[roll_num] == 10:
+        for roll in self._rolls:
+            if roll == 10:
                 strikes_count += 1
         return strikes_count
 
     def _more_than_10_pins_are_rolled_in_this_frame(self, pins):
-        return self._rolls[self._current_roll - 1] + pins > 10
+        return self._rolls[-1] + pins > 10
 
     def _raise_error_if_rolled_more_than_21_times(self):
         if self._rolled_more_than_21_times():
             raise RolledAfter10thFrame
 
     def _rolled_more_than_21_times(self):
-        return self._current_roll > 20
+        return len(self._rolls) > 20
 
     def _raise_error_if_rolled_after_2_non_bonus_rolls_in_10th_frame(self):
         if self._is_3rd_roll_of_10th_frame():
-            has_bonus_for_strike = self._is_strike(self._current_roll - 2)
-            has_bonus_for_spare = self._is_spare(self._current_roll - 2)
+            has_bonus_for_strike = self._is_strike(len(self._rolls) - 2)
+            has_bonus_for_spare = self._is_spare(len(self._rolls) - 2)
             if not has_bonus_for_spare and not has_bonus_for_strike:
                 raise RolledAfter10thFrame
 
@@ -75,7 +72,7 @@ class Game(object):
         first_in_frame = 0
         frame = 1
 
-        while frame < 10 and first_in_frame + 2 < self._current_roll:
+        while frame < 10 and first_in_frame + 2 < len(self._rolls):
 
             if self._is_strike(first_in_frame):
                 first_in_frame += 1
@@ -84,7 +81,7 @@ class Game(object):
                 first_in_frame += 2
                 frame += 1
 
-        return frame == 10 and first_in_frame + 2 == self._current_roll
+        return frame == 10 and first_in_frame + 2 == len(self._rolls)
 
     def _is_strike(self, first_in_frame):
         return self._rolls[first_in_frame] == 10
